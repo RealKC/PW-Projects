2bccecd1a4f6a143f5c8d20f6e614b877dde5629
Dumitru Mițca <dumitru.mitca@student.tuiasi.ro>
2024-03-20 13:47:09 +0200

    Project1: Lab4: Start working on section 2

--- Project1/js/script.js
+++ Project1/js/script.js
@@ -1,9 +1,10 @@
 window.addEventListener("load", () => {
     initializeSidebar();
     initializeSection1();
+    initializeSection2();
 });
 
-const NUMBER_OF_SECTIONS = 1;
+const NUMBER_OF_SECTIONS = 2;
 
 function initializeSidebar() {
     for (let i = 0; i < NUMBER_OF_SECTIONS; ++i) {
@@ -30,4 +31,40 @@ Timpul curent este: <div id="section-1-time">${dateFormat.format(now)}</div>
     setInterval(() => {
         document.getElementById("section-1-time").textContent = dateFormat.format(new Date());
     }, 1000)
+}
+
+function initializeSection2() {
+    const canvas = document.getElementById("jspaint");
+    /** @type {CanvasRenderingContext2D} */
+    const ctx = canvas.getContext("2d");
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#000000";
+    ctx.lineWidth = 2;
+
+    for (const pickerType of ["fill", "stroke"]) {
+        let colorPicker = document.getElementById(`section-2-${pickerType}`);
+        colorPicker.addEventListener("change", (event) => {
+            ctx[`${pickerType}Style`] = event.target.value;
+        });
+        ctx[`${pickerType}Style`] = colorPicker.defaultValue;
+    }
+
+    let firstCoordX = null;
+    let firstCoordY = null;
+
+    canvas.addEventListener("pointerdown", (event) => {
+        const rect = canvas.getBoundingClientRect();
+        const x = (event.x - rect.left) * canvas.width / rect.width;
+        const y = (event.y - rect.top) * canvas.height / rect.height;
+        if (firstCoordX) {
+            console.log(`stroking rect: ${firstCoordX} ${firstCoordY}, w=${x - firstCoordX}, h=${y + firstCoordY}`)
+            ctx.strokeRect(firstCoordX, firstCoordY, x - firstCoordX, y - firstCoordY);
+            firstCoordX = null;
+            firstCoordY = null;
+        } else {
+            firstCoordX = x;
+            firstCoordY = x;
+        }
+    });
 }

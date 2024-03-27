0b17b208cac0d9a3d3ab098ae12854fbc4968083
Dumitru Mițca <dumitru.mitca@student.tuiasi.ro>
2024-03-27 17:51:25 +0200

    Project1: Lab6: Make the server multithreaded

--- Project1/server_web/server_web.py
+++ Project1/server_web/server_web.py
@@ -1,8 +1,7 @@
-import gzip
-from io import BytesIO, StringIO
 import os
 import socket
 import logging
+import threading
 
 from response import Response
 
@@ -39,8 +38,8 @@ def client_supports_gzip(request_headers: str) -> bool:
 
     return request_headers[accept_encoding_start:accept_encoding_start+accept_encoding_end].find('gzip') != -1
 
-def handle_get(client: socket.socket, path: str, supports_gzip: bool):
-    LOG.info(f'Client supports gzip? {supports_gzip}')
+def handle_get(log: logging.Logger, client: socket.socket, path: str, supports_gzip: bool):
+    log.info(f'Client supports gzip? {supports_gzip}')
     for filename in os.listdir('../continut'):
         if filename == path:
             response = Response()
@@ -50,7 +49,7 @@ def handle_get(client: socket.socket, path: str, supports_gzip: bool):
             response.send_to(client)
             return
         elif os.path.isdir(f'../continut/{filename}') and path.startswith(filename):
-            LOG.info(f'returning data from a subdirectory: {filename} {path}')
+            log.info(f'returning data from a subdirectory: {filename} {path}')
             response = Response()
             response.set_200_ok()
             with open(f'../continut/{path}', 'rb') as file:
@@ -62,6 +61,35 @@ def handle_get(client: socket.socket, path: str, supports_gzip: bool):
     not_found.set_404_not_found()
     not_found.send_to(client)
 
+def client_connection_handler(client_socket):
+    request = ''
+    first_line = ''
+
+    log = logging.getLogger(f'{__name__}/web/{threading.current_thread().name}');
+
+    while True:
+        data = client_socket.recv(1024)
+        request += data.decode()
+        log.info(f'S-a citit mesajul: \n------------------------------------\n{request}\n------------------------------------')
+        pozitie = request.find('\r\n')
+        if pozitie > -1:
+            first_line = request[:pozitie]
+            log.info(f'S-a citit linia de start din cerere: ### {first_line} ###')
+            break
+
+    log.info('S-a terminat citirea datelor receptionate')
+
+    (verb, path, _http_version) = get_request_data(first_line)
+
+    log.name += f' ~> {verb} {path}'
+
+    if verb == 'GET':
+        handle_get(log, client_socket, path, client_supports_gzip(request))
+
+    client_socket.close()
+
+    log.info('S-a terminat comunicarea cu clientul')
+
 def main_loop(server_socket: socket.socket):
     while True:
         LOG.info('############################################################')
@@ -70,29 +98,12 @@ def main_loop(server_socket: socket.socket):
         (client_socket, address) = server_socket.accept()
         LOG.info(f'S-a conectat clientul cu adresa: {address}')
 
-        request = ''
-        first_line = ''
+        threading.Thread(
+            target=client_connection_handler,
+            args=[client_socket],
+            name=f'Client {address}'
+        ).start()
 
-        while True:
-            data = client_socket.recv(1024)
-            request += data.decode()
-            LOG.info(f'S-a citit mesajul: \n------------------------------------\n{request}\n------------------------------------')
-            pozitie = request.find('\r\n')
-            if pozitie > -1:
-                first_line = request[:pozitie]
-                LOG.info(f'S-a citit linia de start din cerere: ### {first_line} ###')
-                break
-
-        LOG.info('S-a terminat citirea datelor receptionate')
-
-        (verb, path, _http_version) = get_request_data(first_line)
-
-        if verb == 'GET':
-            handle_get(client_socket, path, client_supports_gzip(request))
-
-        client_socket.close()
-
-        LOG.info('S-a terminat comunicarea cu clientul')
 
 if __name__ == '__main__':
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)

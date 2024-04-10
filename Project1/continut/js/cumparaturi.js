4d40cb0819bd47930b40fd145271368081826810
Dumitru Mițca <dumitru.mitca@student.tuiasi.ro>
2024-04-10 12:23:56 +0300

    Project1: Lab8: Finish 'Tema 1'

--- Project1/continut/js/cumparaturi.js
+++ Project1/continut/js/cumparaturi.js
@@ -1,19 +1,36 @@
 class Product {
     /**
+     * @param {number} id - The ID of the product
      * @param {string} name - The name of the product
      * @param {number} quantity - The number of products the user is buying
      */
-    constructor(name, quantity) {
+    constructor(id, name, quantity) {
+        this.id = id;
         this.name = name;
         this.quantity = quantity;
     }
 }
 
+const PRODUCTS_KEY = 'products';
+
 function addProduct() {
-    let name = document.getElementById('productName').value;
-    let quantity = Number.parseInt(document.getElementById('quantity').vlaue);
+    const name = document.getElementById('productName').value;
+    const quantity = Number.parseInt(document.getElementById('quantity').value);
 
-    const product = new Product(name, quantity);
+    const rawProducts = localStorage.getItem(PRODUCTS_KEY);
+    const products = rawProducts != null ? JSON.parse(rawProducts) : [];
+    const product = new Product(getNewProductId(products), name, quantity);
+    products.push(product);
+    localStorage.setItem(PRODUCTS_KEY, JSON.stringify(products));
+}
 
-    console.log(`${product.name} ${product.quantity}`)
+/**
+ * Returns an ID suitable for a new product
+ * @param {Product[]} products
+ * @returns {number} - the ID
+ */
+function getNewProductId(products) {
+    products.sort((a, b) => a.id - b.id);
+
+    return products[products.length - 1].id;
 }

bb59642e049ea77e8b13a9048f134d675e484651
Dumitru Mițca <dumitru0mitca@gmail.com>
2024-04-30 13:07:20 +0300

    Project2: Lab11: Do 'Tema 2'

--- Project2/app.js
+++ Project2/app.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const express = require('express')
-const expressLayouts = require('express-ejs-layouts')
-const bodyParser = require('body-parser')
-const fs = require('fs')
-const path = require('path')
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
@@ -15,26 +15,38 @@ app.use(expressLayouts);
 app.use('/public', express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.set('layout extractStyles', true);
 
 app.get('/', (req, res) => {
     res.locals = {
         title: 'MițFood'
     };
-    res.render('index');
+
+    res.render('index', {
+        username: req.cookies.username,
+    });
 });
 
 app.get('/auth', (req, res) => {
     res.locals = {
         title: "Autentificare"
     };
-    res.render('auth')
+    res.render('auth', {
+        errorMessage: req.cookies.errorMessage,
+    });
 });
 
 app.post('/verify-auth', (req, res) => {
     console.log(`verify-auth ${req.body} ${req.body.username} ${req.body.password}`);
 
-    res.redirect('/');
+    if (req.body.username === 'user' && req.body.password === '1234') {
+        res.cookie('username', req.body.username);
+        res.redirect('/');
+    } else {
+        res.cookie('errorMessage', 'Credențiale greșite');
+        res.redirect('/auth');
+    }
 });
 
 app.get('/chestionar', (req, res) => {
